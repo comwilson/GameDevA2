c10716dd1d7f89327195c5c683cd314e090e276c
Cameron Wilson <comwilson@gmail.com>
2015-03-13 01:16:09 -0400

    added another level

---
+++ RollerBall/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportController : MonoBehaviour {
+
+	public GameObject ParticleEmitter;
+
+	public Vector3 TeleportCoordinates;
+
+	private GameObject teleportTarget;
+	// Use this for initialization
+	void Start () {
+		ParticleEmitter.SetActive (false);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		ParticleEmitter.SetActive (true);
+
+		Invoke ("Teleport", 0.5f);
+
+		teleportTarget = col.gameObject;
+	}
+
+	void Teleport()
+	{
+		teleportTarget.transform.position = TeleportCoordinates;
+	}
+}
