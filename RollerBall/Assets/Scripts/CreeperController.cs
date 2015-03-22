a97ae817859e0d84ad59d387541fd454a506d9f8
unknown <comwilson@gmail.com>
2015-03-22 10:14:45 -0400

    Added new files

---
+++ RollerBall/Assets/Scripts/CreeperController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreeperController : MonoBehaviour {
+	public GameObject target;
+	public GameObject spotlight;
+	public GameObject playerLight;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spotlight.transform.LookAt (target.transform.position);
+		playerLight.GetComponent<Light> ().intensity = 0.05f;
+		transform.LookAt (target.transform.position);
+	}
+}
